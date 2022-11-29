8ec21d09c35a6b69d81e439881be1bbe16a65715
mtownsend509 <mgtownsend509@gmail.com>
2022-11-28 22:35:55 -0800

    stuff

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ ${data.description}
 1. [Description](#description)
 2. [Installation](#installation)
 3. [Usage](#usage)
-4. [Contributing](#constributing)
+4. [Contributing](#contributing)
 5. [Tests](#tests)
 6. [Questions](#questions)
 
